b0eeac28d2a324cbc151e0b2af50276e99cfe0f4
RedVelocity <blooshaanster@gmail.com>
2024-10-24 22:56:48 +0200

    add precip

--- app/components/weather/DailyWeather.tsx
+++ app/components/weather/DailyWeather.tsx
@@ -1,6 +1,6 @@
-import { FormattedData } from '~/lib/types/weather';
-import dayjsExtended from '~/lib/utils/dayjsExtended';
-import WeatherIcon from '~/components/Icon/WeatherIcon';
+import { FormattedData } from "~/lib/types/weather";
+import dayjsExtended from "~/lib/utils/dayjsExtended";
+import WeatherIcon from "~/components/Icon/WeatherIcon";
 
 const DailyWeather = ({ weather }: { weather: FormattedData }) => {
   const { daily, timezone: TZ } = weather;
@@ -12,20 +12,18 @@ const DailyWeather = ({ weather }: { weather: FormattedData }) => {
       <div className="grid gap-2 mt-4 text-center capitalize sm:grid-cols-2">
         {daily.map((item, i) => (
           <div
-            className="flex gap-1 p-4 justify-between card surface"
+            className="flex lg:grid grid-cols-2 gap-4 p-4 justify-between card surface"
             key={`summary-${i}`}
           >
-            <div className="flex items-center justify-between gap-3">
-              <div className="flex items-center gap-3">
-                <WeatherIcon icon={item.weather.icon} size="medium" />
-                <h4 className="leading-4">
-                  {i !== 0
-                    ? dayjsExtended
-                        .tz(dayjsExtended.unix(item.dt), TZ)
-                        .format('ddd DD')
-                    : 'Today'}
-                </h4>
-              </div>
+            <div className="grid grid-cols-2 md:flex items-center justify-around gap-2">
+              <WeatherIcon icon={item.weather.icon} size="medium" />
+              <h4 className="leading-4 text-left">
+                {i !== 0
+                  ? dayjsExtended
+                      .tz(dayjsExtended.unix(item.dt), TZ)
+                      .format("ddd DD")
+                  : "Today"}
+              </h4>
               <div className="flex items-center gap-1 text-secondary dark:text-secondary-dark">
                 <img
                   src="/assets/icons/rain_drops.png"
